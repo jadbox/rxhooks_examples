f196828f76eed581f8c1cd1627da8b667839a2ac
Jonathan Dunlap <jadit2@gmail.com>
2019-02-07 11:32:20 -0800

    working example (#1)
    * wip
    
    * cleanup + some docs

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,80 @@
 import React, { Component, useState, useEffect, useRef, useCallback, useReducer } from 'react';
 import './App.css';
-import { delay, take, shareReplay, map, reduce } from 'rxjs/operators';
-import { from, interval, Observable, of } from 'rxjs';
-import { useFetch } from 'react-hooks-fetch';
+// import { useFetch } from 'react-hooks-fetch';
+import { scan } from 'rxjs/operators';
+import { interval, Observable, fromEvent, OperatorFunction } from 'rxjs';
+import { EventEmitter } from 'events';
 
-function incCount(setFn:(f:any)=>void, count:any) {
-  return () => setFn(count+1)
-}
-
-function useRx<T, X>(stream: (c:X) => Observable<T>, data:X, fn?:any) {
-  
-  
-  // By default, replace data from last stream
-  if(!fn) fn = (state:any, next:any) => next;
-
-  // Reduce over streams source changes
-  function reducer(state:any, action:any) {
-    switch (action.type) {
-      case 'step':
-        return fn(state, action.val);
-      default:
-        return state;
-    }
-  }
-  // input
-  const [state, dispatch] = useReducer(reducer, data);
-
-  // Final Output
-  const [_state, _setState] = useState<T | undefined>(undefined);
-  
+/*
+  useRx is a hook that takes a Observable (via a function that returns one) and useRx returns [currentStreamOutput] to use the output value in your component.
+  The stream will rerun anytime the second paramater "data" is changed.
+  onErr and onComplete parameters are callbacks for those stream states
+*/
+function useRx<T, X>(stream: (c:X) => Observable<T>, initialValue:X, onErr?:(error: any) => void, onComplete?:()=>void):[T] { 
+  // Output state
+  const [outState, _setOutState] = useState<T | undefined>(undefined);
 
   useEffect( () => {
-    console.log('new stream')
-    const s = stream(state)
-      .subscribe( x => _setState(x) );
+    const s = stream(initialValue)
+      .subscribe( x => _setOutState(x), onErr, onComplete );
     return () => s.unsubscribe();
-  }, [stream, state]);
+  }, [initialValue]);
   
-  return [state, (x:any) => dispatch({type:'step', val: x})];
+  return [outState as T];
 }
 
-function stream(c:number) {
-  // return of(c);
-  return interval(1000*c);
-}
+/*
+  useRxState allows adding items to an Rx stream that is created for you.
+  The pipes parameter allows passing in Rx operators to work with internal source stream.
+  onErr and onComplete parameters are callbacks for those stream states
+*/
+function useRxState<T, X>(initialValue:X, pipes:OperatorFunction<any,any>, onErr?:(error: any) => void, onComplete?:()=>void):[T,(x:X) => void] { 
+  // input to stream
+  const emitterRef = useRef<EventEmitter | null>(null);
 
-function HelloWorld() {
-  const [speed, setSpeed] = useState(1);
-  const [count] = useRx( stream, speed );
+  // Output state
+  const [state, _setState] = useState<T | undefined>(undefined);
+
+  useEffect( () => {
+    emitterRef.current = new EventEmitter();
+    var input$ = fromEvent(emitterRef.current, 'event');
+    const s = input$
+              .pipe(pipes)
+              .subscribe((x:any)=>_setState(x));
+    emitterRef.current!.emit('event', initialValue);
+    return () => s.unsubscribe();
+  }, [initialValue]);
   
-  return <>
-    <button onClick={() => setSpeed(speed+1)}>Slower</button>
-    <p>speed {speed}{' '}|{' '}count {count}</p>
-  </>
+  const setRx = (x:any) => {
+    emitterRef.current!.emit('event', x)
+  }
+  return [state as T, setRx];
 }
 
-function HelloWorld3() {
-  const reducer = (prev:any, val:any) => prev + val;
-  const [count, signalCount] = useRx( stream, 1, reducer );
+function ExampleUseRxState() {
+  const [count, signalCount] = useRxState(1 as number, 
+    scan( (acc:any, x:any)=>x+acc, 0) 
+  );
 
   const onClick = () => {
     signalCount(1);
   }
   
   return <>
-    <button onClick={onClick}>Slower</button>
+    <button onClick={onClick}>Add 1</button>
     <p>count {count}</p>
+  </>
+}
+
+function ExampleUseRx() {
+  const stream = (x:any) => interval(1000 * x);
+
+  const [speed, setSpeed] = useState(1);
+  const [count] = useRx( stream, speed );
+  
+  return <>
+    <button onClick={() => setSpeed(speed+1)}>Make slower via initialValue change</button>
+    <p>speed {speed}{' '}|{' '}count {count}</p>
   </>
 }
 
@@ -75,7 +84,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <HelloWorld3/>
+          <ExampleUseRx/>
+          <br/>
+          <ExampleUseRxState/>
         </header>
       </div>
     );
@@ -83,62 +94,3 @@ class App extends Component {
 }
 
 export default App;
-
-function HelloWorld2() {
-  const [speed, setSpeed] = useState(0.5);
-  const [count] = useRxCallback( speed, stream, map((x:any)=>x*2) );
-  
-  return <>
-    <button onClick={incCount(setSpeed, speed)}>Slower</button>
-    <p>speed {speed}{' '}|{' '}count {count}</p>
-  </>
-}
-
-function useRxCallback2<T, X>(data:X) {
-  const [state, setState] = useState<any>(null);
-  const [obs, obsState] = useState<X | null>(null);
-
-  let stream = (x:X) => {
-    obsState(x);
-  };
-  useEffect( () => {
-    if(!obs) return;
-    const s = of(obs).subscribe( x => setState(x) );
-    return () => s.unsubscribe();
-  }, [data, obs]);
-  
-  return [state, stream]
-}
-
-function useRxCallback<T, X>(data:X, stream: (c:X) => Observable<T>, transform: any) {
-  const [state, setState] = useState<any>(null);
-  const ref = useRef<any>(null);
-
-  useEffect(() => {
-    ref.current = transform;
-  });
-
-  useEffect( () => {
-    const src = stream(data);
-    const op = ref.current ? 
-        src.pipe(ref.current)
-      : src;
-
-    const s = op.subscribe( x => setState(x) )
-    return () => s.unsubscribe();
-  }, [data, transform]);
-  
-  return [state]
-}
-
-function useRx2<T, X>(stream: (c:X) => Observable<T>, data:X) {
-  const [state, setState] = useState<any>(null);
-
-  useEffect( () => {
-    console.log('useEffect')
-    const s = stream(data).subscribe( x => setState(x) );
-    return () => s.unsubscribe();
-  }, [data, stream]);
-  
-  return [state]
-}
