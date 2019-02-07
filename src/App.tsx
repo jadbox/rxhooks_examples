8bdaba6b5a2e2b77507369dd31ee43c8ca325e6d
Jonathan Dunlap <jdunlap@outlook.com>
2019-02-07 11:07:17 -0800

    working

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,64 @@
 import React, { Component, useState, useEffect, useRef, useCallback, useReducer } from 'react';
 import './App.css';
-import { delay, take, shareReplay, map, reduce } from 'rxjs/operators';
-import { from, interval, Observable, of } from 'rxjs';
+import { delay, take, shareReplay, map, reduce, zip, mergeMap, tap, scan } from 'rxjs/operators';
+import { from, interval, Observable, of, fromEvent } from 'rxjs';
 import { useFetch } from 'react-hooks-fetch';
+import { EventEmitter } from 'events';
 
 function incCount(setFn:(f:any)=>void, count:any) {
   return () => setFn(count+1)
 }
 
-function useRx<T, X>(stream: (c:X) => Observable<T>, data:X, fn?:any): [T, (x:X) =>void] {
-  
-  
-  // By default, replace data from last stream
-  if(!fn) fn = (state:any, next:any) => next;
+function useRxState<T, X>(d:X, pipes:any) { 
+  // input to stream
+  const ref = useRef<EventEmitter | null>(null);
 
-  // Reduce over streams source changes
-  function reducer(state:any, action:any) {
-    switch (action.type) {
-      case 'step':
-        const step = fn(state, action.val);
-        console.log('new state', step);
-        return step;
-      default:
-        return state;
-    }
-  }
-  // input
-  const [state, dispatch] = useReducer(reducer, data);
+  // const [state, setState] = useState(d);
 
   // Final Output
   const [_state, _setState] = useState<T | undefined>(undefined);
-  
+  // console.log('_state', _state)
+
+  useEffect( () => {
+
+  }, [_state]);
+  // let em:any;
+  // let input$:Observable<any>;
 
   useEffect( () => {
     console.log('new stream')
-    const s = stream(state)
-      .subscribe( x => _setState(x) );
+    ref.current = new EventEmitter();
+    var input$ = fromEvent(ref.current, 'event');
+    const s = input$
+             // .pipe(tap(x=>console.log('steam$',x)))
+              .pipe(pipes)
+              .subscribe((x:any)=>_setState(x)); // .pipe(pipes$())
+    ref.current!.emit('event', d); // em.emit('event', 1);
     return () => s.unsubscribe();
-  }, [stream, state]);
+  }, []);
   
-  const setRx = (x:any) => dispatch({type:'step', val: x});
-  return [_state as T, setRx];
-}
-
-function stream(c:number) {
-  // return of(c);
-  return interval(1000).pipe(map( (x:number) => x + c )); // .pipe(map(x=>'c ' + c + ' x ' + x));
-}
-
-function HelloWorld() {
-  const [speed, setSpeed] = useState(1);
-  const [count] = useRx( stream, speed );
-  
-  return <>
-    <button onClick={() => setSpeed(speed+1)}>Slower</button>
-    <p>speed {speed}{' '}|{' '}count {count}</p>
-  </>
+  const setRx = (x:any) => {
+    console.log('emit', x)
+    ref.current!.emit('event', x)
+    // setState(x);
+  }
+  // setState(x);
+  return [_state as T, setRx] as [T, typeof setRx];
 }
 
 function HelloWorld3() {
-  const reducer = (prev:any, val:any) => val;
-  const [count, signalCount] = useRx( stream, 1 as number, reducer );
-  if(!count) return null;
+  const [count, signalCount] = useRxState(1 as number, 
+    scan( (acc:any, x:any)=>x+acc, 0) 
+  );
+
+  // const [counta, signalCounta] = useRx(1 as number, 
+   //  (c:any) => reduce( (acc:any, x:any)=>x+acc, 0) 
+  // );
+  // if(!count) return null;
 
   const onClick = () => {
-    console.log('1 + count', 1 + count)
-    signalCount(10 + count);
+    // console.log('1 + count', 1 + count)
+    signalCount(1);
   }
   
   return <>
@@ -81,6 +74,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <HelloWorld3/>
+          <HelloWorld/>
         </header>
       </div>
     );
@@ -89,61 +83,32 @@ class App extends Component {
 
 export default App;
 
-function HelloWorld2() {
-  const [speed, setSpeed] = useState(0.5);
-  const [count] = useRxCallback( speed, stream, map((x:any)=>x*2) );
+function HelloWorld() {
+  const stream = (x:any) => interval(1000 * x);
+
+  const [speed, setSpeed] = useState(1);
+  const [count] = useRx( stream, speed );
   
   return <>
-    <button onClick={incCount(setSpeed, speed)}>Slower</button>
+    <button onClick={() => setSpeed(speed+1)}>Slower</button>
     <p>speed {speed}{' '}|{' '}count {count}</p>
   </>
 }
 
-function useRxCallback2<T, X>(data:X) {
-  const [state, setState] = useState<any>(null);
-  const [obs, obsState] = useState<X | null>(null);
+function useRx<T, X>(stream: (c:X) => Observable<T>, data:X, onErr?:any, onComplete?:any): [T] {
+  // input
+  // const [state, setState] = useState(data);
+  // console.log('speed', state)
 
-  let stream = (x:X) => {
-    obsState(x);
-  };
-  useEffect( () => {
-    if(!obs) return;
-    const s = of(obs).subscribe( x => setState(x) );
-    return () => s.unsubscribe();
-  }, [data, obs]);
+  // Final Output
+  const [_state, _setState] = useState<T | undefined>(undefined);
   
-  return [state, stream]
-}
-
-function useRxCallback<T, X>(data:X, stream: (c:X) => Observable<T>, transform: any) {
-  const [state, setState] = useState<any>(null);
-  const ref = useRef<any>(null);
-
-  useEffect(() => {
-    ref.current = transform;
-  });
 
   useEffect( () => {
-    const src = stream(data);
-    const op = ref.current ? 
-        src.pipe(ref.current)
-      : src;
-
-    const s = op.subscribe( x => setState(x) )
+    const s = stream(data)
+      .subscribe( x => _setState(x), onErr, onComplete );
     return () => s.unsubscribe();
-  }, [data, transform]);
+  }, [data]);
   
-  return [state]
-}
-
-function useRx2<T, X>(stream: (c:X) => Observable<T>, data:X) {
-  const [state, setState] = useState<any>(null);
-
-  useEffect( () => {
-    console.log('useEffect')
-    const s = stream(data).subscribe( x => setState(x) );
-    return () => s.unsubscribe();
-  }, [data, stream]);
-  
-  return [state]
+  return [_state as T];
 }
